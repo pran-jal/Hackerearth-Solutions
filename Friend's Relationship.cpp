158dfacbbd355604b23a6d87e19745b75cb11079
pran-jal <72604669+pran-jal@users.noreply.github.com>
2021-01-30 13:21:57 -0600

    Update Friend's Relationship.cpp

--- Friend's Relationship.cpp
+++ Friend's Relationship.cpp
@@ -2,17 +2,17 @@
 int main()
 {
 	int t,n,i,j;
-	scanf("%d",&t);
+	scanf("%d",&t);   //no. of test cases
 	while(t)
 	{
-		scanf("%d",&n);
+		scanf("%d",&n); //no. of rows
 		for(i=1; i<=n; i++)
 		{
-			for(j=0; j<i; j++)
+			for(j=0; j<i; j++)            // *'s before the first # of each row
 				printf("%c",'*');
-			for(j=1; j<=2*(n-i); j++)
+			for(j=1; j<=2*(n-i); j++)     // all #'s of the row 
 				printf("%c",'#');
-			for(j=0; j<i; j++)
+			for(j=0; j<i; j++)            // *'s after the last #
 				printf("%c",'*');
 			printf("\n");			
 		}
