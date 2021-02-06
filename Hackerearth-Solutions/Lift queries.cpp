a4ab7557c59bc273e5156ec22a1004ec7211ab38
pran-jal <72604669+pran-jal@users.noreply.github.com>
2021-02-06 08:22:33 -0600

    Create Lift queries.cpp

---
+++ Hackerearth-Solutions/Lift queries.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int t, a=0, b=7, n;
+	scanf("%d",&t);
+	while(t)
+	{
+		scanf("%d",&n);
+		if(abs(a-n)<abs(b-n))
+		{
+			printf("%c\n",'A');
+			a=n;
+		}
+		else if(abs(a-n)>abs(b-n))
+		{
+			printf("%c\n",'B');
+			b=n;
+		}
+		else if(abs(a-n)==abs(b-n))
+		{
+			if(a<b)
+			{	printf("%c\n",'A');
+				a=n;
+			}
+			else
+			{
+				printf("%c\n",'B');
+				b=n;
+			}
+		}
+
+		t--;
+	}
+}
