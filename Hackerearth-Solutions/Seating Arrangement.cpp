787490e4088ffd071a096ecd997427fe438f8924
pran-jal <72604669+pran-jal@users.noreply.github.com>
2021-02-06 08:15:02 -0600

    Create Seating Arrangement.cpp

---
+++ Hackerearth-Solutions/Seating Arrangement.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int t, stno;
+	int r,q,temp;
+	scanf("%d",&t);
+	while(t)
+	{	char *ch;
+		scanf("%d",&stno);
+		r=stno%12;
+		q=stno/12;
+		if(r==0)
+		{	r=12;
+			q--;
+		}
+		printf("%d",12*q+13-r);
+		temp=r;
+		r=temp%3;
+		q=temp/3;
+		if(r!=2)
+		{if(q==1 || q==3)
+			r=0;
+		else 
+			r=1;
+		}
+		if(r==0)
+			ch="AS";
+		else if(r==1)
+			ch="WS";
+		else
+			ch="MS";
+		printf(" %s\n",ch);
+		t--;
+	}
+}
