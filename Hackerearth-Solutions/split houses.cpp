129776a5ce68a2a2ce454b1ce82042eb5985bc96
pran-jal <72604669+pran-jal@users.noreply.github.com>
2021-02-06 08:18:20 -0600

    Create split houses.cpp

---
+++ Hackerearth-Solutions/split houses.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n, i, count=0, flag=0;
+	scanf("%d",&n);
+	char c[20];
+	scanf("%s",&c);
+	for(i=0; i<n; i++)
+	{
+		if(c[i]!='H')
+			c[i]='B';
+		else
+		{	count++;
+			flag=i;
+			if(c[i+1]=='H')
+			{	flag=1;
+				break;
+			}
+		}
+	}
+	if((count==n && count!=1) || flag==1)
+		printf("%s\n","NO");
+	else
+	{	printf("%s\n","YES");
+		printf("%s\n",c);}
+}
