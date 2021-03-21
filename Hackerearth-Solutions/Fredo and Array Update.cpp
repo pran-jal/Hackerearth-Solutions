80c28302c823b173e1d8cf23eda9a7162c5f049a
Pranjal <72604669+pran-jal@users.noreply.github.com>
2021-03-21 03:07:01 -0600

    Create Fredo and Array Update.cpp

---
+++ Hackerearth-Solutions/Fredo and Array Update.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  long n, i, sum=0;
+  scanf("%ld",&n);
+  int a[n];
+  for(i=0; i<n; i++){
+    scanf("%d",&a[i]);
+    sum+=a[i];
+  }
+  printf("%ld", (sum/n)+1);
+  return 0;
+}
